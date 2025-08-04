72602b6f72875d8fbd64868c343806c7f109b21d
mohannautiyal <mohannautiyal80@gmail.com>
2025-08-04 21:36:42 +0530

    data structures list

---
+++ Datastructures/ListDS.py
@@ -0,0 +1,24 @@
+# data structures
+
+# If we want to represent a group of individual objects as a single entity where
+# insertion order is preserved and duplicates are allowed, than we go for list
+# Heterogeneous objects
+# Dynamic, Mutable
+# index --> +ve and -ve
+# Square braces --> individual objects
+
+list_1 = []  #empty list
+print(list_1)
+print(type(list_1))
+# ======================================
+
+mylist= ["Madan",45,"Fidelity"]
+# ======================================
+
+listnum = list(range(2,14,3))
+print(listnum)
+# ======================================
+python_string = "Learning python list"
+lststrg= python_string.split(" ")
+
+print(lststrg)
